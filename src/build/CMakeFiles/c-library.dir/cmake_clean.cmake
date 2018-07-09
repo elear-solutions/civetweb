d6c835698e5f24ca87943815c69b50f37eb243de
Lakshminarayana Gurram <lakshminarayana@elear.solutions>
2018-07-09 17:08:30 +0530

    first commit

---
+++ src/build/CMakeFiles/c-library.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c-library.dir/civetweb.o"
+  "libcivetweb.pdb"
+  "libcivetweb.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c-library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
